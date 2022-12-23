63c148fdba4b35fbaf6ddfe35604190d676cd52e
LeeJeongGi <apple4rhk@naver.com>
2022-12-24 00:15:18 +0900

    feat : exception class 추가

---
+++ src/main/java/com/project/api/common/handler/GeneralExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.project.api.common.handler;
+
+import com.project.api.common.exception.NotFoundException;
+import com.project.api.common.exception.UnauthorizedException;
+import com.project.api.common.utils.ApiUtils;
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import static com.project.api.common.utils.ApiUtils.error;
+
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
+        return newResponse(throwable.getMessage(), status);
+    }
+
+    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(String message, HttpStatus status) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<>(error(message, status), headers, status);
+    }
+
+    @ExceptionHandler({
+            NoHandlerFoundException.class,
+            NotFoundException.class,
+    })
+    public ResponseEntity<?> handleNotFoundException(Exception e) {
+        return newResponse(e, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<?> handleUnauthorizedException(Exception e) {
+        return newResponse(e, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({
+            IllegalArgumentException.class,
+            IllegalStateException.class,
+            ConstraintViolationException.class,
+            MethodArgumentNotValidException.class,
+    })
+    public ResponseEntity<?> handleBadRequestException(Exception e) {
+        logger.debug("Bad request exception occurred: {}", e.getMessage(), e);
+        if (e instanceof MethodArgumentNotValidException) {
+            return newResponse(
+                    ((MethodArgumentNotValidException) e).getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+
+        return newResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public ResponseEntity<?> handleException(Exception e) {
+        logger.error("Unexpected exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
